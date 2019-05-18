6af5e5ae092776a00142b08530028ea106d6ae5d
Jan Martin <martin-jan@arcor.de>
2019-05-18 22:32:37 +0200

    Updating download script to also download velodyne data.

--- get_kitti_data_corpus.sh
+++ get_kitti_data_corpus.sh
@@ -4,13 +4,15 @@ KITTI_BASE_URL=<YOURBASEURL_HERE>
 
 files=(data_object_image_2.zip
 data_object_label_2.zip
+data_object_velodyne.zip
 data_object_calib.zip
-raw_data_downloader.zip)
+raw_data_downloader.zip
+devkit_object.zip)
 
 for i in ${files[@]}; do
 
 	echo "Downloading: "$i
-        wget -c -t 0 -T 8 '$KITTI_BASE_URL/'$i
+        wget -c -t 0 -T 8 $KITTI_BASE_URL/$i
         unzip -o $i
         rm $i
 done
